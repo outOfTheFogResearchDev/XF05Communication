c7dc421e075722700917c79319e3a37c39d130d2
Max <maxl.berkowitz@gmail.com>
2019-02-11 17:20:25 -0800

    beginings

---
+++ server/app/utils/cpp/setUpOIP3.cpp
@@ -0,0 +1,42 @@
+#include <node.h>
+#include "visa.h"
+
+using namespace v8;
+
+void setAnalyzer(const FunctionCallbackInfo<Value> &args)
+{
+    Isolate *isolate = args.GetIsolate();
+    double frequency = args[0].As<Number>()->Value();
+
+    //* C++ starts here
+
+    ViSession defaultRM, viMXA;
+    ViStatus viStatus = 0;
+
+    viStatus = viOpenDefaultRM(&defaultRM);
+    viStatus = viOpen(defaultRM, "GPIB0::16::INSTR", VI_NULL, VI_NULL, &viMXA);
+
+    if (viStatus)
+        return;
+
+    // Set band center and marker
+    viPrintf(viMXA, "FREQ:CENTER %f GHz\n", frequency);
+    viPrintf(viMXA, "FREQ:SPAN 200 KHz\n");
+    viPrintf(viMXA, "DISP:WIND:TRAC:Y:RLEV -50 dBm\n");
+    viPrintf(viMXA, "BAND 1 KHz\n");
+    viPrintf(viMXA, "BAND:VID 100 Hz\n");
+    viPrintf(viMXA, "CALC:MARK1:MODE OFF\n");
+    viPrintf(viMXA, "CALC:MARK1:SET:CENT\n");
+
+    viClose(viMXA);     // closes session
+    viClose(defaultRM); // closes default session
+
+    //* C++ ends here
+}
+
+void init(Local<Object> exports, Local<Object> method)
+{
+    NODE_SET_METHOD(method, "exports", setAnalyzer);
+}
+
+NODE_MODULE(NODE_GYP_MODULE_NAME, init);
